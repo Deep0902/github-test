eec1af3f2facf7f6adf7e68f95f970d26c8e3690
Deep Rakesh <deeptank09@gmail.com>
2026-03-07 15:09:31 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Lenis from "lenis";
+import { useEffect } from "react";
+import MainContent from "./components/MainContent";
+import styles from "./page.module.css";
+import InstallPrompt from "./components/InstallPrompt";
+
+export default function Home() {
+  useEffect(() => {
+    // npm install lenis
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+  return (
+    <div className={styles.page}>
+      <MainContent />
+    </div>
+  );
+}
